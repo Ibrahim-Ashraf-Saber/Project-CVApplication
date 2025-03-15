c7c6c3afee552a229c695272a87c4ebb39bdce06
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2025-03-16 01:52:04 +0200

    Edit Download PDF

--- src/components/SaveResume.jsx
+++ src/components/SaveResume.jsx
@@ -2,26 +2,28 @@ import "../styles/saveResumeStyle.css";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
-const handlePrint = () => {
-  const input = document.getElementById("cv");
+function SaveResume() {
+  const handlePrint = () => {
+    const input = document.getElementById("cv");
 
-  html2canvas(input, {
-    scale: 2, // زيادة الدقة للشاشات عالية الدقة
-    useCORS: true, // إذا كنت تستخدم صور من مصادر خارجية
-  }).then((canvas) => {
-    const imgData = canvas.toDataURL("image/png");
-    const pdf = new jsPDF({
-      orientation: "portrait", // أو 'landscape' إذا كنت تريد اتجاه أفقي
-      unit: "mm",
-      format: "a4", // أو أي حجم آخر تريده
+    html2canvas(input, {
+      scale: 2, // زيادة الدقة للشاشات عالية الدقة
+      useCORS: true, // إذا كنت تستخدم صور من مصادر خارجية
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF({
+        orientation: "portrait", // أو 'landscape' إذا كنت تريد اتجاه أفقي
+        unit: "mm",
+        format: "a4", // أو أي حجم آخر تريده
+      });
+
+      const imgWidth = pdf.internal.pageSize.getWidth();
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      pdf.save("cv.pdf");
     });
-
-    const imgWidth = pdf.internal.pageSize.getWidth();
-    const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-    pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-    pdf.save("cv.pdf");
-  });
+  };
 
   return (
     <div className="containter">
@@ -29,6 +31,6 @@ const handlePrint = () => {
       <button onClick={handlePrint}>Download</button>
     </div>
   );
-};
+}
 
 export default SaveResume;
